46d32a6ffebdfc79ecace2584c1a68c6691c98f7
Chelsea Valentine <cv851@nyu.edu>
2017-09-05 23:13:10 -0400

    Add visualizer route

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = function(app) {
   app.get('/', function(req, res){
     res.redirect('/tests/index.html?hidepassed');
@@ -5,5 +7,9 @@ module.exports = function(app) {
 
   app.get('/tests', function(req, res){
     res.redirect('/tests/index.html');
+  });
+
+  app.get('/visualizer', (req, res) => {
+    res.sendFile(path.resolve('demos/visualizer.html'));
   });
 };
